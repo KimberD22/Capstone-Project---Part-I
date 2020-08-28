1207ab9c272cca43372a7d45a33185b75cd97c07
KimberD22 <kimberdepriest@gmail.com>
2020-08-27 20:42:12 -0400

    broke my app

--- petplanet/src/app/mock-products.ts
+++ petplanet/src/app/mock-products.ts
@@ -1,31 +1,52 @@
 export const products = [
     {
       id: 1,
-      name: '',
-      price: '',
-      description: '',
-      animal: ''
+      name: 'Premium Dog Food',
+      price: 20.00,
+      description: '10lb bag of dog food made with quality ingredients',
+      animal: 'dog'
     },
     {
         id: 2,
-        name: '',
-        price: '',
-        description: '',
-        animal: ''
+        name: 'Premium Cat Food ',
+        price: 10.00,
+        description: '10lb bag of cat food made with quality ingredients',
+        animal: 'cat'
     },
     {
         id: 3,
-        name: '',
-        price: '',
-        description: '',
-        animal: '',
+        name: 'Premium Fish Food',
+        price: 5.00,
+        description: '2lb container of fish flakes made with quality ingredients',
+        animal: 'fish',
     },
     {
       id: 4,
-      name: '',
-      price: '',
-      description: '',
-      animal: '',
+      name: 'Premium Hamster Food',
+      price: 7.00,
+      description: '5lb container of hamster pellets made with quality ingredients',
+      animal: 'pocket pet',
     },
+    {
+        id: 5,
+        name: 'Premium Bird Food',
+        price: 8.00,
+        description: '10lb container of bird seed made with quality ingredients',
+        animal: 'bird', 
+    },
+    {
+        id: 6,
+      name: 'Dog Toy',
+      price: 5.00,
+      description: 'fun, safe toy',
+      animal: 'dog',
+    },
+    {
+    id: 7,
+    name: 'Cat Toy',
+    price: 5.00,
+    description: 'fun, safe toy',
+    animal: 'cat',
+}
 
   ];
